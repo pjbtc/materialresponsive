4d81b45a9224c7d7a7b428c46363f4cabc27c8e0
pjbtc <pjbhattachan@gmail.com>
2021-04-16 01:12:37 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact=()=>{
+	return(
+		<div>
+			<div>
+			<p> Dublin, kaski, pokhara, Nepal, Ireland</p>
+			</div>
+		</div>
+		)
+}
+export default Contact; 
