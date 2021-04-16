27163bc5adb17458a50eea3412fc14c76a17cfbe
pjbtc <pjbhattachan@gmail.com>
2021-04-16 01:11:40 +0100

    Add files via upload

---
+++ Blog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog=()=>{
+	return(
+		<div>
+		<p> Being electronics engineer and webdeveloper , I have passion in writing articles as well. Basically 
+		my articles are related to web development field and other IT sectors. The main goal of these articles is to provide 
+		knowledge and information to the people interested in IT field. </p>
+		</div>
+		)
+}
+export default Blog;
