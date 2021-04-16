02aef985b059784ed8682ba8a23ccc03fbaa1b87
pjbtc <pjbhattachan@gmail.com>
2021-04-16 01:11:11 +0100

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home=()=>{
+	return(
+		<div>
+		<p> This website is simple porfolio website including weather  forcast features.</p>
+		</div>
+		)
+}
+export default Home ;
